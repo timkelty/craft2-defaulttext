12c4b5876f29a45fe83624c0533d0236118543d7
Tim Kelty <tim@fusionary.com>
2017-10-13 13:49:14 -0400

    Fix RichText

--- fieldtypes/DefaultText_RichTextFieldType.php
+++ fieldtypes/DefaultText_RichTextFieldType.php
@@ -5,6 +5,7 @@ class DefaultText_RichTextFieldType extends RichTextFieldType
 {
     use DefaultText_BaseFieldTypeTrait;
 
+    protected static $_redactorLang = 'en';
     protected function getDefaultValueSettingsHtml()
     {
         return craft()->templates->render('defaulttext/_settings', [
